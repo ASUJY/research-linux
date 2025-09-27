b4671a639a73cb357662355dd1f16a092c4ca4bc
ASUJY <prime_sujy@foxmail.com>
2025-09-27 20:24:07 +0800

    修复导致多处静态变量未正确初始化的问题，以及新增代码会导致debug失败的问题，这两个问题都是由于没有正确从硬盘中读取内核到内存中导致的。 通过查看内核相关文件的二进制文件就能够发现，static变量在编译的时候已经正确赋值，但是运行的时候却没有值！ 这都是由于bootsect.asm文件中用CHS方式来读取硬盘的代码有问题，当第三次读取硬盘数据到内存中的时候，发生了磁头的切换，导致数据没有按顺序来读取。 现在改用LBA的方式来读取硬盘中的数据到内存中，已经能够正常运行了！

--- boot/bootsect.asm
+++ boot/bootsect.asm
@@ -3,7 +3,7 @@ INITSEG     equ 0x9000      ;将bootsect复制到段地址0x9000处，即0x90000
 SETUPLEN    equ 4           ;setup扇区数量。
 SETUPSEG    equ 0x9020      ;将setup扇区加载到段地址0x09020处。
 SYSSEG      equ 0x1000      ;将system模块加载到0x10000处
-SYSSIZE     equ 0x3000      ;编译链接后system模块的大小
+SYSSIZE     equ 0x5000      ;编译链接后system模块的大小
 ENDSEG      equ SYSSEG + SYSSIZE
 
 [SECTION .text]
@@ -61,101 +61,83 @@ ok_load_setup:
     int 0x10
 
     ;现在开始将system模块加载到0x10000处，即把硬盘第6个扇区开始往后的240个扇区加载到内存0x10000处
+    ;将硬盘中的内核读取到0x1200开始的内存地址
     mov ax, SYSSEG
     mov es, ax
-    call read_it
+    mov di, 0
+    mov cx, 5      ; 从硬盘中的第3个扇区开始读，硬盘的扇区从0开始编号
+    mov bl, 240      ; 读取60个扇区
+    call read_hd    ; 执行从硬盘指定位置读取数据到指定内存位置中的操作
 
     jmp SETUPSEG:0      ;跳转到setup.asm继续执行
 
-;输入: es - 开始内存地址段值
-read_it:
-    mov ax, es
-    test ax, 0x0fff         ;测试输入的段值。从盘上读入的数据必须存放在位于内存地址0x10000的边界开始处，否则进入死循环
-die:
-    jne die
-    xor bx, bx              ;清空bx寄存器，用于表示当前段内存放数据的开始位置，即段内偏移位置
+;把硬盘中指定的扇区内容读入到指定的内存中,有以下几个入参
+;di = 内存地址  bl = 读多少个扇区
+;ecx = 从第几个扇区开始读数据
+read_hd:
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
 
-;判断是否已经读入全部数据。比较当前所读段是否就是系统数据末端所处的段(ENDSEG)
-;如果不是就跳转至下面ok1_read标号处继续读数据。否则退出子程序返回。
-rp_read:
-    mov ax, es
-    cmp ax, ENDSEG
-    jb ok1_read
-    ret
+    inc dx
+    mov al, cl
+    out dx, al
 
-;计算和验证当前磁道需要读取的扇区数，放在ax寄存器中
-;根据当前磁道还未读取的扇区数以及段内数据字节开始偏移位置，计算如果全部读取这些未读扇区所读总字节数
-;是否会超过64KB段长度的限制。若会超过，则根据此次最多能读入的字节数(64KB-段内偏移位置)，反算出此次需要读取的扇区数
-ok1_read:
-    mov ax, [sectors]   ;取每磁道扇区数
-    sub ax, [sread]     ;减去当前磁道已读取的扇区数
-    mov cx, ax
-    shl cx, 9           ;cx = cx * 512字节
-    add cx, bx          ;cx = cx + 段内当前偏移值(bx) = 此次读操作后，段内共读入的字节数。
-    jnc ok2_read        ;若没有超过64KB字节，则跳转至ok2_read处执行。
-    je ok2_read
-    xor ax, ax          ;若加上此次将要读取的磁道上的所有未读扇区时会超过64KB，则计算此时最多能读入的字节数(64KB-段内读偏移位置)
-    sub ax, bx
-    shr ax, 9           ;再转换成需要读取的扇区数
-ok2_read:
-    call read_track
-    mov cx, ax          ;cx = 该次操作已读取的扇区数
-    add ax, [sread]     ;当前磁道上已经读取的扇区数
-    cmp ax, [sectors]   ;如果当前磁道上还有扇区未读，则跳转到ok3_read处
-    jne ok3_read
-    mov ax, 1           ;读该磁道的下一磁头面(1号磁头)上的数据。如果已经完成，则去读下一磁道。
-    sub ax, [head]      ;判断当前磁头号
-    jne ok4_read        ;如果是0磁头，则再去读1磁头面上的扇区数据
-    inc word [track]    ;否则去读下一磁道
-ok4_read:
-    mov [head], ax      ;保存当前磁头号
-    xor ax, ax          ;清空当前磁道已读扇区数
-ok3_read:
-    mov [sread], ax     ;保存当前磁道已读扇区数
-    shl cx, 9           ;上次已读扇区数*512字节
-    add bx, cx          ;调整当前段内数据开始位置
-    jnc rp_read         ;若小于64KB边界值，则跳转到rp_read(156行)处，继续读数据。否则调整当前段，为读下一段数据做准备
-    mov ax, es
-    add ax, 0x1000      ;将段基址调整为指向下一个64KB内存开始处。
-    mov es, ax
-    xor bx, bx          ;清空段内数据开始偏移值
-    jmp rp_read         ;跳转至rp_read（156行）处，继续读数据。
+    inc dx
+    mov al, ch
+    out dx, al
 
-;读当前磁道上指定开始扇区和需要读取扇区数的数据到es:bx开始处
-;al - 需要读取的扇区数量
-;es:bx - 缓冲区开始位置
-read_track:
-    push ax
-    push bx
+    inc dx
+    shr cx, 16     ; shr：右移指令， 这里是把ecx中的数据右移16位
+    mov al, cl
+    out dx, al
+
+    inc dx
+    mov al, ch
+    or al, 0b1110_0000
+    out dx, al
+
+    inc dx
+    mov al, 0x20
+    out dx, al
+
+    ;读取多个扇区就要循环多少次
+    mov cl, bl
+.start_read:
     push cx
-    push dx
-    mov dx, [track]     ;读取当前磁道号
-    mov cx, [sread]     ;读取当前磁道上已读的扇区数量
-    inc cx              ;cl = 开始读取的扇区号
-    mov ch, dl          ;ch = 当前磁道号
-    mov dx, [head]      ;获取当前磁头号
-    mov dh, dl          ;dh = 磁头号
-    mov dl, 0x80        ;dl = 驱动器号(0x80表示当前读取硬盘)
-    and dx, 0x0180      ;磁头号不大于1
-    mov ah, 2           ;ah = 2, 读磁盘扇区功能号
-    int 0x13
-    jc bad_rt           ;若出错，则跳转至bad_rt
-    pop dx
+    call .wait_hd_prepare       ;等待数据准备完毕
+    call read_hd_data           ;读取一个扇区
     pop cx
-    pop bx
-    pop ax
+    loop .start_read
+.return:
     ret
 
-;执行驱动器复位操作(磁盘中断功能号0)，再跳转到read_track处重试
-bad_rt:
-    mov ax, 0x0000
-    mov dx, 0x0080
-    int 0x13
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    jmp read_track
+; 一直等待，直到硬盘的状态是：不繁忙，数据已准备好
+; 即第7位为0，第3位为1，第0位为0
+.wait_hd_prepare:
+    mov dx, 0x1f7
+.check:
+    in al, dx
+    and al, 0b1000_1001
+    cmp al, 0b0000_1000
+    jnz .check
+    ret
+
+; 读硬盘，一次读两个字节，读256次，刚好读一个扇区
+read_hd_data:
+    mov dx, 0x1f0
+    mov cx, 256
+.read_word:
+    in ax, dx
+    mov [es:di], ax
+    add di, 2
+    jnc .no_segment_adjust  ; 检查是否跨越段边界
+    mov ax, es
+    add ax, 0x1000  ;调整es寄存器到下一个内存段，因为一个内存段最大映射64KB地址，并且di最大值也是64KB
+    mov es, ax
+.no_segment_adjust:
+    loop .read_word
+    ret
 
 
 
